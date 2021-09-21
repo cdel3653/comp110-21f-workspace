0ad2c85829350604e9e5dd993c2b1845f9f35e5b
Cassidy Dellinger <cdel3653@live.unc.edu>
2021-09-21 11:09:19 -0400

    remainders exercise

--- exercises/ex03/tar_heels.py
+++ exercises/ex03/tar_heels.py
@@ -7,6 +7,6 @@ int_prompt = int(input("Enter an int: "))
 if int_prompt % 2 != 0 and int_prompt % 7 != 0:
     print("CAROLINA")
 if int_prompt % 2 == 0:
-    print("TAR ",end="")
-if int_prompt % 7 ==0:
+    print("TAR ", end="")
+if int_prompt % 7 == 0:
     print("HEELS")
