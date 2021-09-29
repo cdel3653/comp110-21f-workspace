24aafd3f4fbfe92cc70bdd4fd5498e852a2d22b2
Cassidy Dellinger <cdel3653@live.unc.edu>
2021-09-28 21:19:55 -0400

    progress on ex04

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -5,13 +5,16 @@ __author__ = "730320843"
 
 def all(xs: list[int], check: int) -> bool:
     """Do all the ints match?"""
+    if len(xs) == 0:
+        return False
     for x in xs:
         if x != check:
             return False
     return True
-    
+
 
 def is_equal(xs: list[int], ys: list[int]) -> bool:
+    """Do list ints equate?"""
     if len(xs) != len(ys):
         return False
 
@@ -25,23 +28,13 @@ def is_equal(xs: list[int], ys: list[int]) -> bool:
 
 
 def max(xs: list[int]) -> int:
+    """Return max val of lists."""
     if len(xs) == 0:
         raise ValueError("max() arg is an empty List")
-    maxx = 0
+    maxx = xs[0]
     i = 0
     while i < len(xs):
         if maxx < xs[i]:
             maxx = xs[i]
         i += 1
     return maxx
-
-
-
-
-
-
-
-
-
-
-
