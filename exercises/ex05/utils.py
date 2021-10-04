b1e1a6efdbd07edc378553a9c82e8ef5805b77aa
Cassidy Dellinger <cdel3653@live.unc.edu>
2021-10-04 19:39:40 -0400

    ex05 progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,46 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730320843"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]):
+    """Return even ints from a list."""
+    if len(xs) == 0:
+        return []
+    evens = []
+    for x in xs:
+        if x % 2 == 0:
+            evens.append(x)
+    return evens
+
+
+def sub(xs: list[int], start: int, end: int):
+    """Create subset list."""
+    if len(xs) == 0 or start > len(xs) or end <= 0 or start >= end:
+        return []
+    if start < 0:
+        start = 0
+    if end > len(xs):
+        end = len(xs)
+    counter = start
+    sublist = []
+    while counter < end:
+        sublist.append(xs[counter])
+        counter += 1
+    return sublist
+
+
+def concat(xs: list[int], ys: list[int]):
+    """Concat some lists."""
+    if len(xs) == 0:
+        return ys
+    if len(ys) == 0:
+        return xs
+    new_list = []
+    for x in xs:
+        new_list.append(x)
+    for y in ys:
+        new_list.append(y)
+    return new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,52 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730320843"
+
+
+def test_evens_empty() -> None:
+    assert only_evens([]) == []
+
+
+def test_evens_list() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    assert only_evens(xs) == [2, 4]
+
+
+def test_evens_all_evens() -> None:
+    xs: list[int] = [2, 4, 6, 8]
+    assert only_evens(xs) == [2, 4, 6, 8]
+
+
+def test_sub_negative_start() -> None:
+    xs: list[int] = [2, 4, 6, 8]
+    assert sub(xs, -2, 3) == [2, 4, 6]
+
+
+def test_sub_small() -> None:
+    xs: list[int] = [2, 4, 6, 8]
+    assert sub(xs, 1, 2) == [4]
+
+
+def test_sub_large() -> None:
+    xs: list[int] = [2, 4, 6, 8, 10, 12]
+    assert sub(xs, 1, 4) == [4, 6, 8]
+
+
+def test_concat_empty() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    assert concat(xs, []) == [1, 2, 3, 4]
+
+
+def test_concat_small() -> None:
+    xs: list[int] = [1, 2]
+    ys: list[int] = [3, 4]
+    assert concat(xs, ys) == [1, 2, 3, 4]
+
+
+def test_concat_large() -> None:
+    xs: list[int] = [1, 2, 3, 4]
+    ys: list[int] = [5, 6, 7]
+    assert concat(xs, ys) == [1, 2, 3, 4, 5, 6, 7]
